45ee0e7ee249fadd800918ea7b898047e4a27933
Grigorii-Ilin <grigory1728@gmail.com>
2020-04-11 21:04:23 +0300

    rsa-alpha version

---
+++ RSA/my_rsa/mod_ops.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "mod_ops.h"
+
+int mod_add(int x, int y, int p) {
+	if ((p - x) > y)
+		return x + y;
+	else
+		return y - (p - x);
+}
+
+int mod_sub(int x, int y, int p) {
+	return mod_add(x, p - y, p);
+}
+
+int mod_mul(int x, int y, int p) {
+	int x1, x2;
+	int y1, y2;
+	int s = (int)sqrt((double)p);
+	if (x == 0 || y == 0) return 0;
+	if (x == 1) return y;
+	if (y == 1) return x;
+	if (x <= s && y <= s) {
+		return x * y;
+	}
+	else {
+		x1 = x >> 1;
+		x2 = x - x1;
+		y1 = y >> 1;
+		y2 = y - y1;
+		return mod_add(mod_add(mod_add(mod_mul(x1, y1, p), mod_mul(x1, y2, p), p), mod_mul(x2, y1, p), p), mod_mul(x2, y2, p), p);
+	}
+}
+
+int mod_div(int pub, int p) {
+	int i = p;
+	int j = pub;
+	int y = p;
+	int y2 = 0;
+	int y1 = 1;
+	int quotient = p;
+	int remainder = p;
+	if (pub >= p)
+		return p;
+	while (j > 0) {
+		quotient = i / j;
+		remainder = i - (j * quotient);
+		y = y2 - (y1 * quotient);
+		i = j;
+		j = remainder;
+		y2 = y1;
+		y1 = y;
+	}
+	if (i != 1)
+		return p;
+	if (y2 > 0)
+		return y2 % p;
+	else
+		return p + y2;
+}
+
+int mod_pow(int x, int n, int p) {
+	int r = 1;
+	while (n) {
+		if ((n & 1)) r = mod_mul(x, r, p);
+		n >>= 1;
+		x = mod_mul(x, x, p);
+	}
+	return r;
+}
---
+++ RSA/my_rsa/my_rsa.c
@@ -0,0 +1,69 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+#include "mod_ops.h"
+
+int ggT(int a, int b) {
+	int tmp;
+	while (b > 0 && a > 0) {
+		if (b > a) {
+			tmp = a;
+			a = b;
+			b = tmp;
+		}
+		a = a % b;
+	}
+	if (b > a)
+		return b;
+	else
+		return a;
+}
+
+int get_public(int phi) {
+	int public_key;
+	int offset = 0;
+	public_key = rand() % phi;
+	while (ggT(public_key, phi) > 1) {
+		offset++;
+		public_key = (rand() + offset) % phi;
+	}
+	return public_key;
+}
+
+
+int main(void) {
+	int p1 = 227, p2 = 31;
+	int n = p1 * p2;
+	int phi = (p1 - 1) * (p2 - 1);
+	int pub;
+	int priv;
+	int message[6] = { 'h','a','l','l','o',0 };
+	int i = 0;
+	srand(time(0));
+	pub = get_public(phi);
+	priv = mod_div(pub, phi);
+
+	printf("Verschluessele Text...\n");
+	for (i = 0; i < 6; i++) {
+		printf("%c", (char)message[i]);
+		message[i] = mod_pow(message[i], pub, n);
+	}
+	printf(" ----> ");
+	for (i = 0; i < 6; i++) {
+		printf("%c", (char)('!' + (message[i] % ('}' - '!'))));
+	}
+	printf("\n");
+
+	printf("Entschluessele Text...\n");
+	for (i = 0; i < 6; i++) {
+		message[i] = mod_pow(message[i], priv, n);
+		printf("%c", (char)message[i]);
+	}
+	printf("\n");
+
+	printf("Pub: %d Priv: %d Phi: %d\n", pub, priv, phi);
+	printf("%d * %d = %d\n", pub, priv, mod_mul(pub, priv, phi));
+
+	return EXIT_SUCCESS;
+}
